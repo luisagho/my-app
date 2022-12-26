c6ac2b62845578c6270708487737dd305759dc39
luisagho <luisalvarezgho@gmail.com>
2022-12-26 16:09:48 +0100

    filter done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import pokemon from "./pokemon.json";
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
 
 function App() {
+  const [filter, setFilter] = useState("");
+  const handleOnChange = (value) => setFilter(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='title'>Pokemon search</h1>
+      <input
+        value={filter}
+        onChange={event => handleOnChange(event.target.value)}
+      />
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody> {
+          pokemon.slice(0, 20).filter(pokemon =>
+            pokemon.name.english.toLowerCase().includes(filter.toLowerCase())).
+            map(pokemon =>
+              <PokemonRow
+                pokemon={pokemon}
+                key={pokemon.id}
+              />
+            )}
+        </tbody>
+      </table>
     </div>
   );
 }
